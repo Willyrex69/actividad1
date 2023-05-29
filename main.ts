a075b2b8ad9e20ab449e200ccab6941431fd0d78
Willyrex69 <134914488+Willyrex69@users.noreply.github.com>
2023-05-29 09:04:38 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Torres")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Felipe")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Parrilla")
+})
+basic.showString("Felipe")
 basic.forever(function () {
 	
 })
